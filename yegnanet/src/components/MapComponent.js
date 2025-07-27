390a41825276d9703e22f1008fa3fe853faf380b
Brook Kassa <brookassa4@gmail.com>
2025-07-27 13:46:27 -0400

    Updated Tile Styles

--- yegnanet/src/components/MapComponent.js
+++ yegnanet/src/components/MapComponent.js
@@ -43,8 +43,10 @@ function MapComponent({ hotspots }) {
         style={{ height: "500px", width: "100%" }}
       >
         <TileLayer
-          attribution='&copy; <a href="https://osm.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution='&copy; <a href="https://carto.com/">CARTO</a>'
+          url="https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png"
+
+
         />
 
         {hotspots.map((spot) => {
